4c958ede68d296742c9ed3c12b65fc6fb01045d1
Purity <ronopurity05@gmail.com>
2018-09-30 16:33:17 +0300

    i renamed the professional shop.html

--- db.con.php
+++ db.con.php
@@ -2,9 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "software engineering"
+$dbname = "software engineering";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-?>
