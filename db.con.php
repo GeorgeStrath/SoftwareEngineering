8f3823d60b6b0752c684f73e992c7a16e9ba0f60
NgugiMacharia <dennis.ngugi@strathmore.edu>
2018-10-01 12:00:45 +0300

    corrected db.con.php

--- db.con.php
+++ db.con.php
@@ -2,11 +2,9 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-<<<<<<< HEAD
 $dbname = "professionals_shop";
-=======
-$dbname = "software engineering";
->>>>>>> 4c958ede68d296742c9ed3c12b65fc6fb01045d1
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+?>
